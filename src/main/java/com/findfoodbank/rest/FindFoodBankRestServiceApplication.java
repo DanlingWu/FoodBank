aac4fb96faca12634ee75eb3d4783cd6451ef6ec
Danling Wu <danling.wu.927@gmail.com>
2020-01-06 19:48:41 +0000

    remove unused imports

--- src/main/java/com/findfoodbank/rest/FindFoodBankRestServiceApplication.java
+++ src/main/java/com/findfoodbank/rest/FindFoodBankRestServiceApplication.java
@@ -4,11 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.task.TaskExecutor;
-import org.springframework.scheduling.annotation.AsyncConfigurerSupport;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-
-import java.util.concurrent.Executor;
 
 @SpringBootApplication
 @EnableAsync
